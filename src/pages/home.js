55be22a8829e7fe459a8c719ef1c34cce5d93f6b
Ahmad Marzuki <ahmad.marzuki@grosirparts.com>
2021-10-15 16:34:33 +0700

    Add outer border

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,14 +5,16 @@ import CompaniesView from '../components/CompaniesView'
 const Home = () => {
   return (
     <div className='container px-36 py-12'>
-      <div className='flex flex-wrap justify-between'>
-        <CompanyForm />
-        <div className='my-4 border border-l border-r-0 border-gray-300 min-h-full' />
-        <OfficeForm />
+      <div className='border rounded p-4'>
+        <div className='flex flex-wrap justify-between'>
+          <CompanyForm />
+          <div className='my-4 border border-l border-r-0 border-gray-300 min-h-full' />
+          <OfficeForm />
+        </div>
+        <hr className='mt-8' />
+        <h1 className='text-3xl my-4'>Companies</h1>
+        <CompaniesView />
       </div>
-      <hr className='mt-8' />
-      <h1 className='text-3xl my-4'>Companies</h1>
-      <CompaniesView />
     </div>
   )
 }
