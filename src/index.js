d3e13c02e2780537f0bb7b471f0964ced7c1e63e
Ahmad Marzuki <ahmad.marzuki@grosirparts.com>
2021-10-15 10:34:34 +0700

    Add form component library

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { store } from './redux/store'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import 'react-datepicker/dist/react-datepicker.css'
 
 ReactDOM.render(
   <React.StrictMode>
