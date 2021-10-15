8d76b1a107a92daacafd358cf6821bc675fd59d8
Ahmad Marzuki <ahmad.marzuki@grosirparts.com>
2021-10-15 16:34:48 +0700

    View company info

---
+++ src/components/OfficeCard.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+const OfficeCard = ({ id, name, address, revenue, phoneCode, phoneNumber }) => {
+  return (
+    <Link to={`/companies/${id}`}>
+      <div className='max-w-md border border-gray-400 rounded p-2'>
+        <div className='text-lg font-bold'>{name}</div>
+        <hr className='' />
+        <div className='my-4'>
+          <p className='title'>Address:</p>
+          <p className='description'>{address}</p>
+        </div>
+        <div className='my-4'>
+          <p className='title'>Revenue:</p>
+          <p className='description'>{revenue}</p>
+        </div>
+        <div className='my-4'>
+          <p className='title'>Phone No.:</p>
+          <p className='description'>
+            ({phoneCode}) {phoneNumber}
+          </p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+OfficeCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  revenue: PropTypes.number.isRequired,
+  phoneCode: PropTypes.string.isRequired,
+  phoneNumber: PropTypes.string.isRequired,
+}
+
+export default OfficeCard
--- src/pages/company.js
+++ src/pages/company.js
@@ -1,5 +1,52 @@
+import { useParams, Link } from 'react-router-dom'
+import { useGetOfficesQuery } from '../redux/services/company'
+
 const Company = () => {
-  return <div>company page with its offices</div>
+  const { id } = useParams()
+
+  const { data, error, isLoading } = useGetOfficesQuery(+id)
+
+  return (
+    <div className='container px-36 py-12'>
+      <div className='border rounded p-4'>
+        {error ? (
+          <p>something went wrong...</p>
+        ) : isLoading ? (
+          <p>data is loading...</p>
+        ) : data.companyData.length === 0 ? (
+          <p>There is no such company in the database...</p>
+        ) : (
+          <div>
+            <h1 className='text-2xl font-semibold'>{data.companyData.name}</h1>
+            <hr className='mt-2' />
+            <div className='my-4'>
+              <h2 className='text-md font-bold'>Address: </h2>
+              <p className='text-lg'>{data.companyData.address}</p>
+            </div>
+            <div className='my-4'>
+              <h2 className='text-md font-bold'>Revenue: </h2>
+              <p className='text-lg'>{data.companyData.revenue}</p>
+            </div>
+            <div className='flex justify-between'>
+              <div className='my-4'>
+                <h2 className='text-md font-bold'>Phone No.: </h2>
+                <p className='text-lg'>
+                  ({data.companyData.phoneCode}) {data.companyData.phoneNumber}
+                </p>
+              </div>
+              <Link to='/'>
+                <button className='border rounded px-6 py-2 self-end bg-gray-300 hover:bg-yellow-100'>
+                  Back to Overview
+                </button>
+              </Link>
+            </div>
+            <hr className='my-4' />
+            <h1 className='text-2xl'>Offices</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Company
