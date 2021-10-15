e7d6004a8ba085b07f8ce991a110775708de5277
Ahmad Marzuki <ahmad.marzuki@grosirparts.com>
2021-10-15 14:39:58 +0700

    Implement invalidating data on mutation

--- src/redux/services/company.js
+++ src/redux/services/company.js
@@ -4,18 +4,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const companyApi = createApi({
   reducerPath: 'companyApi',
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  tagTypes: ['Company', 'Office'],
   endpoints: (builder) => ({
     getCompanies: builder.query({
       query: () => ({ url: 'companies' }),
+      providesTags: ['Company'],
     }),
     getOffices: builder.query({
       query: (companyId) => ({ url: `offices/${companyId}` }),
+      providesTags: ['Office'],
     }),
     addCompany: builder.mutation({
       query: (body) => ({ url: 'companies', method: 'POST', body }),
+      invalidatesTags: ['Company'],
     }),
     addOffice: builder.mutation({
       query: (body) => ({ url: 'offices', method: 'POST', body }),
+      invalidatesTags: ['Office'],
     }),
   }),
 })
